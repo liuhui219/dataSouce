c4276510cceb825951b660f3e26f5201df9a9233
liuhui219 <5372397+liuhui219@user.noreply.gitee.com>
2020-11-16 14:41:21 +0800

    sdfdsfs

---
+++ cdn.js
@@ -0,0 +1,10 @@
+(function (window) {
+  window.dataCdn = {
+    data: "html",
+    note: "此为备用CDN，暂时未使用",
+    "index.html": "/index.html",
+    react: "https://unpkg.com/react@16/umd/react.development.js",
+    "react-dom": "https://unpkg.com/react-dom@16/umd/react-dom.development.js",
+    babel: "https://unpkg.com/babel-standalone@6.15.0/babel.min.js",
+  };
+})(window);
