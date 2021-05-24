668071d4f74f3872154da4a94cd53e7ca5e778ce
liuhui219 <liuhui219@126.com>
2021-05-24 22:03:56 +0800

    1111

--- cdn.js
+++ cdn.js
@@ -7,4 +7,10 @@
     "react-dom": "https://unpkg.com/react-dom@16/umd/react-dom.development.js",
     babel: "https://unpkg.com/babel-standalone@6.15.0/babel.min.js",
   };
+  let a = 100000000;
+  let sum = 0
+  for(var i = 0;i<a;i++){
+    sum += i
+  }
+  window.aaa = sum
 })(window);
